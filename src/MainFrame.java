66019732e1a78827d0e686867e73c4c9e4596afd
32d <105021055@live.asia.edu.tw>
2018-01-02 01:48:12 +0800

    3

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -2,8 +2,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Random;
 
@@ -13,8 +11,11 @@ public class MainFrame extends JFrame  {
     private int scH = Toolkit.getDefaultToolkit().getScreenSize().height;
     private int W = 500;
     private int H = 500;
+    private JLabel Jlab = new JLabel();
+    private JLabel Jlab2 = new JLabel();
     static final int SIZE = 15;
     private JPanel JPL;
+    private JPanel food;
     private LinkedList<JPanel> snakeBody = new LinkedList();
     private Snake  snake ;
     public JPanel getJPL() {
@@ -23,6 +24,7 @@ public class MainFrame extends JFrame  {
     public LinkedList<JPanel> getSnakeBody() {
         return snakeBody;
     }
+
     public void initSnakePanel(){
         JPanel body1 = new JPanel();
         body1.setBounds(0, 0, SIZE, SIZE);
@@ -30,20 +32,17 @@ public class MainFrame extends JFrame  {
         snakeBody.add(body1);
         for ( int i = 0; i < 4; i++) {
             JPanel body = new JPanel();
-            JPanel last = snakeBody.getLast();
-            body.setBounds(last.getX()+ SIZE+1, 0, SIZE, SIZE);
-            body.setBackground(Color. red);
             snakeBody.add(body);
             JPL.add(body);
         }
         JPL.add(body1);
     }
-    private void myActiongLisenler(){
+    private void KEYLisenler(){
         this.addKeyListener( new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
                 int code = e.getKeyCode();
-                if(code == 39 && snake == null){
+                if(code == 10 && snake == null){
                     snake = new Snake(MainFrame. this);
                     snake.start();
                 }
@@ -53,12 +52,30 @@ public class MainFrame extends JFrame  {
             }
         });
     }
+    private void Food(){
+        food = new JPanel();
+        food.setBackground(Color. black);
+        Random rdm = new Random();
+        int x = rdm.nextInt( SIZE)*16;
+        int y = rdm.nextInt( SIZE)*16;
+        food.setBounds(x, y, SIZE, SIZE);
+        JPL.add(food);
+    }
     public MainFrame(){
         init();
         setTitle( "貪吃蛇");
         initSnakePanel();
-        myActiongLisenler();
-      //  this.setSize(500, 500);
+        Food();
+        KEYLisenler();
+        JPL.add(Jlab);
+        JPL.add(Jlab2);
+        Jlab.setText("按ENTER後來開始遊戲");
+        Jlab2.setText("控制方法↑ ↓ ← → ");
+        Jlab.setBounds(150,0,500,500);
+        Jlab2.setBounds(150,50,500,500);
+        Jlab.setFont(new Font("標楷體", Font.BOLD, 16));
+        Jlab2.setFont(new Font("標楷體", Font.BOLD, 16));
+
         this.setLocationRelativeTo( null);
         this.setVisible( true);
         setResizable(false);
--- src/Snake.java
+++ src/Snake.java
@@ -1,6 +1,6 @@
 import java.awt.*;
 import java.util.LinkedList;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 public class Snake extends Thread{
     private MainFrame snake;
@@ -9,18 +9,14 @@ public class Snake extends Thread{
     public int getWay() {
         return way;
     }
-
     public void setWay( int way) {
         this. way = way;
     }
-
     public Snake(MainFrame snake) {
         this. snake = snake;
     }
-
     @Override
     public void run() {
-        //循環的移動，循環一次走一步，并且延時
         while( true){
             move();
             try {
@@ -32,38 +28,35 @@ public class Snake extends Thread{
     }
     private void move(){
         LinkedList<JPanel> snakeBody = snake.getSnakeBody();
-
-        JPanel JPL = snake.getJPL();
-        //獲取到舊頭
-        JPanel oldHead = snakeBody.getLast();
-        //獲取舊頭的坐標
+        JPanel JPL = snake.getJPL();//取舊頭
+        JPanel oldHead = snakeBody.getLast();//取舊頭坐標
         int x = oldHead.getX();
         int y = oldHead.getY();
         switch( way){
             case 37:  //往左
                 x = x - ( snake. SIZE + 1);
                 break;
-            case 38:    //往下
+            case 38:    //往上
                 y = y - ( snake. SIZE + 1);
                 break;
             case 39:    //往右
                 x = x + snake. SIZE + 1;
                 break;
-            case 40:    //往上
+            case 40:    //往下
                 y = y + snake. SIZE + 1;
                 break;
         }
         JPanel newHead = new JPanel();//創建新頭
         newHead.setBounds(x, y, snake. SIZE, snake. SIZE);
         newHead.setBackground(Color. red);
+
+
         snakeBody.add(newHead);
         JPL.add(newHead);
         JPL.remove(snakeBody.getFirst());//去尾
         snakeBody.removeFirst();
 
-       if(x==-1){
 
-       }
         snake.repaint();
     }
 }
